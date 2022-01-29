eff3c87d3a420b8d02aac163ba4d85ca9e2b063a
henryque <henryque_1@hotmail.com>
2022-01-29 12:02:39 -0400

    chore(ShowUserProfileUseCase.spec): Add show users profile test

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Show User Profile", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    showUserProfileUseCase = new ShowUserProfileUseCase(
+      usersRepositoryInMemory
+    );
+  });
+
+  it("Should be able to show a user profile", async () => {
+    const user = await createUserUseCase.execute({
+      name: "User Test",
+      email: "user@test.com",
+      password: "1234",
+    });
+
+    const showUserProfile = await showUserProfileUseCase.execute(user.id);
+
+    expect(showUserProfile).toHaveProperty("id");
+  });
+});
