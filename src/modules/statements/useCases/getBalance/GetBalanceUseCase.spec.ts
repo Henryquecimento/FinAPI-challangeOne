a48283e163ade2820f26cd775b30d1683185d31f
henryque <henryque_1@hotmail.com>
2022-01-30 12:49:38 -0400

    chore(GetBalanceUseCase.spec): add GetBalance test

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let createUsersUseCase: CreateUserUseCase;
+let statementsRepositoryInMemory: InMemoryStatementsRepository;
+let getBalanceUseCase: GetBalanceUseCase;
+
+describe("Get Balance", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUsersUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    statementsRepositoryInMemory = new InMemoryStatementsRepository();
+    getBalanceUseCase = new GetBalanceUseCase(
+      statementsRepositoryInMemory,
+      usersRepositoryInMemory
+    );
+  });
+
+  it("Should be able to get balance", async () => {
+    const user = await createUsersUseCase.execute({
+      name: "User Test",
+      email: "user@test.com",
+      password: "1234",
+    });
+
+    const balance = await getBalanceUseCase.execute({
+      user_id: user.id,
+    });
+
+    expect(balance).toHaveProperty("statement");
+    expect(balance).toHaveProperty("balance");
+  });
+});
