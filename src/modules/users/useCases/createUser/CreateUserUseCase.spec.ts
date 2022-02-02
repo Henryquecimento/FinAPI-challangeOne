89a6e2aad094c06f23cb99fde2f5c706dd7f6c63
henryque <henryque_1@hotmail.com>
2022-02-02 12:27:25 -0400

    chore(CreateUserUseCase.spec): add user already exists validation test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let usersRepositoryInMemory: InMemoryUsersRepository;
@@ -18,5 +19,21 @@ describe("Create a new User", () => {
     });
 
     expect(user).toHaveProperty("id");
+  });
+
+  it("Should not be able to create a user if email already exists", () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@test.com",
+        password: "1234",
+      });
+
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@test.com",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
   });
 });
