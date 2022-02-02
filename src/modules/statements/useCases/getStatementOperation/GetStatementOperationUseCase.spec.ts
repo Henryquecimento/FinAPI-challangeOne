007b84e33ca65766f2aab13d3efb4982a63eb5ee
henryque <henryque_1@hotmail.com>
2022-02-02 12:14:46 -0400

    chore(GetStatementOperationUseCase.spec): add non-existent validations tests

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -3,6 +3,7 @@ import { InMemoryStatementsRepository } from "@modules/statements/repositories/i
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
 import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase";
+import { GetStatementOperationError } from "./GetStatementOperationError";
 import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase";
 
 let usersRepositoryInMemory: InMemoryUsersRepository;
@@ -40,12 +41,12 @@ describe("Get Statement Operation", () => {
       description: "Add test deposit",
     });
 
-    const operationStatement = await getStatementOperationUseCase.execute({
+    const statementOperation = await getStatementOperationUseCase.execute({
       user_id: user.id,
       statement_id: depositStatement.id,
     });
 
-    expect(operationStatement).toHaveProperty("id");
+    expect(statementOperation).toHaveProperty("id");
   });
 
   it("Should be able to get a withdraw statement operation", async () => {
@@ -69,11 +70,46 @@ describe("Get Statement Operation", () => {
       description: "Withdraw test",
     });
 
-    const operationStatement = await getStatementOperationUseCase.execute({
+    const statementOperation = await getStatementOperationUseCase.execute({
       user_id: user.id,
       statement_id: withdrawStatement.id,
     });
 
-    expect(operationStatement).toHaveProperty("id");
+    expect(statementOperation).toHaveProperty("id");
+  });
+
+  it("Should not be able to get a statement operation for non-existent user", async () => {
+    expect(async () => {
+      const depositStatement = await statementRepositoryInMemory.create({
+        user_id: "nonExistent_user_id",
+        type: OperationType.DEPOSIT,
+        amount: 1000,
+        description: "Add test deposit",
+      });
+
+      if (!depositStatement.id) fail("Statement Id is probably undefined");
+
+      await getStatementOperationUseCase.execute({
+        user_id: "nonExistent_user_id",
+        statement_id: depositStatement.id,
+      });
+    }).rejects.toBeInstanceOf(GetStatementOperationError.UserNotFound);
+  });
+
+  it("Should not be able to get a non-existent statement operation from user", async () => {
+    expect(async () => {
+      const user = await createUsersUseCase.execute({
+        name: "User Test",
+        email: "user@test.com",
+        password: "1234",
+      });
+
+      if (!user.id) fail("User Id is probably undefined");
+
+      await getStatementOperationUseCase.execute({
+        user_id: user.id,
+        statement_id: "nonExistent_statement_id",
+      });
+    }).rejects.toBeInstanceOf(GetStatementOperationError.StatementNotFound);
   });
 });
