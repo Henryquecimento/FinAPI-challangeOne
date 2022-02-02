85ec0116f6481e3ae2d1518d1d79d18ae00c3808
henryque <henryque_1@hotmail.com>
2022-02-02 11:03:50 -0400

    chore(CreateStatementUseCase.spec): add non existent user validation test

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -2,6 +2,7 @@ import { OperationType } from "@modules/statements/enums/OperationType";
 import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
+import { CreateStatementError } from "./CreateStatementError";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 let usersRepositoryInMemory: InMemoryUsersRepository;
@@ -20,7 +21,18 @@ describe("Create a Statement", () => {
     );
   });
 
-  it("Should be able to make a deposit", async () => {
+  it("Should not be able to create a statement for a non-existent user", () => {
+    expect(async () => {
+      await createStatementUseCase.execute({
+        user_id: "nonExistent_user_id",
+        type: OperationType.DEPOSIT,
+        amount: 1000,
+        description: "Add test deposit",
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
+  });
+
+  it("Should be able to create a statement with deposit type", async () => {
     const user = await createUsersUseCase.execute({
       name: "User Test",
       email: "user@test.com",
@@ -39,7 +51,7 @@ describe("Create a Statement", () => {
     expect(deposit).toHaveProperty("amount");
   });
 
-  it("Should be able to make a withdraw", async () => {
+  it("Should be able to create a statement with withdraw type", async () => {
     const user = await createUsersUseCase.execute({
       name: "User Test",
       email: "user@test.com",
