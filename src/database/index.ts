2ba0e40e619bd139a2914b13646a5d03d0af5bcd
henryque <henryque_1@hotmail.com>
2022-02-16 14:11:03 -0400

    fix(database/index): readjust database configurations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,10 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
-export default async () => {
+export default async (host = "database") => {
   const options = await getConnectionOptions();
 
   Object.assign(options, {
+    host,
     database: process.env.NODE_ENV === "test" ? "fin_api" : options.database,
   });
 
